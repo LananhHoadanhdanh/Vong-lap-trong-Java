fc01008fce3e5cbc8b9e4841caad282e738635e9
Phạm Thị Lan Anh <90228386+LananhHoadanhdanh@users.noreply.github.com>
2021-10-07 23:49:14 +0700

    prime print

---
+++ prime_print/src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+    public static boolean isPrimeNumber(int n) {
+        if (n < 2) {
+            return false;
+        }
+        int squareRoot = (int) Math.sqrt(n);
+        for (int i = 2; i <= squareRoot; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập vào số lượng cần in ra");
+        int number = scanner.nextInt();
+        for (int i = 2; i < number; i++) {
+            if (isPrimeNumber(i)) {
+                System.out.println(i);
+            }
+        }
+    }
+}
